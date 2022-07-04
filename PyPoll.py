0800a53d360f713d580e6686443352174ff9b841
Cullen Weber <webercullen@gmail.com>
2022-07-04 16:05:16 -0600

    update CSV reader for election-analysis through 3.4.2

--- PyPoll.py
+++ PyPoll.py
@@ -4,3 +4,15 @@
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of th eelection based on popular vote.
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'resources/election_results.csv'
+
+# Open the election results and read the file.
+election_data = open(file_to_load,'r')
+
+# To do: perform analysis.
+
+
+# Close the file.
+election_data.close()
