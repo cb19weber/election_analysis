69283a84b7ae74004ee9274b582625d66d2444b6
Cullen Weber <webercullen@gmail.com>
2022-07-04 20:12:22 -0600

    update folder structure and output files

--- PyPoll.py
+++ PyPoll.py
@@ -5,14 +5,23 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of th eelection based on popular vote.
 
+# Import modules
+import csv
+import os
+
 # Assign a variable for the file to load and the path.
-file_to_load = 'resources/election_results.csv'
+file_to_load = os.path.join("resources","election_results.csv")
+file_to_save = os.path.join("analysis","election_analysis.txt")
 
 # Open the election results and read the file.
-election_data = open(file_to_load,'r')
+# election_data = open(file_to_load,'r')
+with open(file_to_load) as election_data:
 
 # To do: perform analysis.
+    print(election_data)
 
+# Using the open() function with the 'w' mode we will write data to the file.
+open(file_to_save,"w")
 
 # Close the file.
-election_data.close()
+# election_data.close()
