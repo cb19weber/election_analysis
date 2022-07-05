2fa0765348d6e730096eafbe1135af02f9fd7327
Cullen Weber <webercullen@gmail.com>
2022-07-04 20:47:43 -0600

    update module through 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -5,23 +5,36 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of th eelection based on popular vote.
 
-# Import modules
+# Add dependencies (import modules)
 import csv
 import os
 
-# Assign a variable for the file to load and the path.
+# Assign variables for the file to load and the file to save and the paths.
 file_to_load = os.path.join("resources","election_results.csv")
 file_to_save = os.path.join("analysis","election_analysis.txt")
 
 # Open the election results and read the file.
-# election_data = open(file_to_load,'r')
+# Files can be opened like this:
+# open(file_to_load, "r")
 with open(file_to_load) as election_data:
 
-# To do: perform analysis.
-    print(election_data)
+# Perform analysis: read and analyze the data here.
+    file_reader = csv.reader(election_data)
+    headers = next(file_reader)
+    print(headers)
+    # for row in file_reader:
+    #    print(row)
 
-# Using the open() function with the 'w' mode we will write data to the file.
-open(file_to_save,"w")
+# Write data analysis to the created file.
+# Files can be opened like this:
+# open(file_to_save, "w")
+with open(file_to_save,"w") as txt_file:
+    txt_file.write("Counties in the Election\n")
+    txt_file.write("-"*25 + "\n")
+    txt_file.write("Arapahoe\n")
+    txt_file.write("Denver\n")
+    txt_file.write("Jefferson")
 
 # Close the file.
 # election_data.close()
+# outfile.close()
