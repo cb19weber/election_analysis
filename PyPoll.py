e97a56af09bfb98355e391ecc00db084286f82ee
Cullen Weber <webercullen@gmail.com>
2022-07-06 23:04:57 -0600

    completed M 3.5.6 through election results

--- PyPoll.py
+++ PyPoll.py
@@ -46,7 +46,34 @@ with open(file_to_load) as election_data:
         #4. Accumulate the total votes for each candidate.
         candidate_votes[candidate_name] += 1
 
-print(candidate_votes)
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+# Determine the percentage of votes for each candidate by looping through the counts.
+# 1. Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    # 2. Retrieve vote count of a candidate:
+    votes = candidate_votes[candidate_name]
+    # 3. Calulate the percentage of votes:
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # 4. Print the candidate name and percentage of votes:
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})")
+    # 5. Determine the winning candidate:
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_candidate = candidate_name
+        winning_count = votes
+        winning_percentage = vote_percentage
+
+winning_candidate_summary = (
+    f"----------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"----------------------------\n")
+
+print(winning_candidate_summary)
 
 # Write data analysis to the created file.
 # Files can be opened like this:
@@ -55,7 +82,8 @@ with open(file_to_save,"w") as txt_file:
     txt_file.write("Counties in the Election\n")
     txt_file.write("-"*25 + "\n")
     txt_file.write(f"{total_votes:,}\n")
-    txt_file.write(f"{candidate_options}")
+    txt_file.write(f"{candidate_options}\n")
+
 
 # Close the file.
 # election_data.close()
